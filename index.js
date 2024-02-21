110755437995d2bb663c0d0295e1fce4d7a22272
Kris Janssen Cabansag <kjs.cabansag@gmail.com>
2024-02-21 17:25:29 -0500

    complete first class function lab

---
+++ index.js
@@ -0,0 +1,16 @@
+const receivesAFunction = (callBack) => {
+    return callBack();
+}
+
+
+const returnsANamedFunction = () => {
+    return function namedFuction() {
+        
+}
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {
+        
+    };
+}
